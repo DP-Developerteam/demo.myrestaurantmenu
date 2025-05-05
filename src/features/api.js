12ffd295a958ca3b66a44679326a4a6dc4ec7b5a
Diego Pérez Rodríguez <diego@MacBook-Pro-de-Diego.local>
2025-05-05 18:10:32 +0200

    proxy vercel

--- src/features/api.js
+++ src/features/api.js
@@ -7,12 +7,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
     timeout: 10000,
-    withCredentials: true,
-    // FIXING OAUTH:
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
+    withCredentials: true
 });
 
 // Function to set the Authorization header with the token
